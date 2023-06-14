d046d0c39e68da7fa9531bd223d9caf5672c8826
Bobbert <Robert.pallesen13@gmailcom>
2023-06-14 11:24:30 +0200

    added endpoint to msssql stroed procedure

--- mssql.py
+++ mssql.py
@@ -9,47 +9,49 @@ load_dotenv()
 db_pw = os.getenv("MSSQL_PW")
 
 # Endpoint for executing the stored procedure
-@app.route('/api/getUsers', methods=['POST'])
+@app.route('/api/executeStoredProc', methods=['POST'])
 def execute_stored_procedure():
     # Parse the request data
-    user_id = request.get_json('user_id')
+    request_data = request.get_json()
+    user_id = request_data['userID']
+    order_lines = request_data['orderLines']
 
     # Establish a connection to the SQL Server database
     conn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};'
-    'SERVER=localhost;'
-    'PORT=1433;'
-    'DATABASE=DBExam;'
-    'UID=sa;'
-    'PWD=' + db_pw + ';'
-)
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=localhost;'
+        'PORT=1433;'
+        'DATABASE=DBExam;'
+        'UID=sa;'
+        'PWD=' + db_pw + ';'
+    )
 
     # Create a cursor object to execute SQL statements
     cursor = conn.cursor()
 
     try:
+        # Generate the INSERT INTO statements for each order line
+        insert_statements = []
+        for line in order_lines:
+            product_id = line['product_id']
+            product_name = line['product_name']
+            quantity = line['quantity']
+            price = line['price']
+
+            insert_statement = f"INSERT INTO @typeOrderLines(productID, productName, quantity, price) VALUES ({product_id}, '{product_name}', {quantity}, {price})"
+            insert_statements.append(insert_statement)
+
+        # Combine the INSERT INTO statements into a single SQL query
+        insert_query = "; ".join(insert_statements)
+
         # Execute the stored procedure
-        cursor.execute("{CALL GetUsers(?)}", user_id)
-
-        # Fetch the result set
-        result_set = cursor.fetchall()
-
-        # Process the result set
-        users = []
-        for row in result_set:
-            user = {
-                'user_id': row.UserID,
-                'username': row.UserName,
-                'email': row.Email,
-                'address': row.Address
-            }
-            users.append(user)
+        cursor.execute(f"DECLARE @typeOrderLines OrderLinesType, @userID int; SET @userID = ?; {insert_query}; EXEC dbo.InsertOrderWithOrderLines @userID, @typeOrderLines", user_id)
 
         # Commit the transaction
         conn.commit()
 
         # Return the response as JSON
-        return jsonify(users)
+        return jsonify({'message': 'Stored procedure executed successfully'})
 
     except Exception as e:
         # Rollback the transaction in case of any error
